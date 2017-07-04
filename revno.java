c27fe827e0d8373319c54c813d20d77980e278a3
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-04 09:19:26 +0530

    Add files via upload

---
+++ revno.java
@@ -0,0 +1,19 @@
+package guvi;
+import java.util.Scanner;
+public class revno {
+	public static void main(String args[])
+    {
+        int n, m, a = 0,x;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any number:");
+        n = s.nextInt();
+        m = n;
+        while(n > 0)
+        {
+            x = n % 10;
+            a = a * 10 + x;
+            n = n / 10;
+        }
+        System.out.println(a);
+    }
+}
